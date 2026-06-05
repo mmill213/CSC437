64e685789dbdcb0dc5fcb72e5c3a5221d1fb8fbe
Micah Miller <mmill213@calpoly.edu>
2026-06-05 15:54:41 -0700

    fit fix4

--- packages/app/src/views/home-view.ts
+++ packages/app/src/views/home-view.ts
@@ -51,7 +51,7 @@ export class HomeViewElement extends HTMLElement {
             margin-bottom: 0.5rem;
           }
 
-          @media screen and (max-width: 70rem) {
+          @media screen and (max-width: 150rem) {
             :host {
               grid-template-columns: repeat(2, 1fr);
             }
