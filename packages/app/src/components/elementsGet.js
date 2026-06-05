277c33074c08ce9a43867b8ebe174be58c4d53c8
Micah Miller <mmill213@calpoly.edu>
2026-06-05 15:57:20 -0700

    fit fix5

--- packages/app/src/components/elementsGet.js
+++ packages/app/src/components/elementsGet.js
@@ -61,7 +61,7 @@ export class ZoneElement extends HTMLElement {
     .zone-dashboard {
       grid-column: 1 / -1;
       display: grid;
-      grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+      grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
       gap: 1rem;
       padding: 1rem;
       width: 100%;
