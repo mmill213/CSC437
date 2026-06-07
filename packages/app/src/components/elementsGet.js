e98e952868fa7082aad07887f467d46c7de0189a
Micah Miller <mmill213@calpoly.edu>
2026-06-07 15:11:45 -0700

    new edit icon

--- packages/app/src/components/elementsGet.js
+++ packages/app/src/components/elementsGet.js
@@ -239,7 +239,7 @@ function renderZoneSection(zoneItem, zoneReadingData, index) {
         </svg>
 
         <span class="zone-name-display">${name}</span>
-        <button class="edit-btn" type="button" title="Edit name">&#x1F589</button>
+        <button class="edit-btn" type="button" title="Edit name">&#x270f</button>
 
         <form class="zone-edit-form" style="display:none">
           <input type="text" name="name" value=${name} />
